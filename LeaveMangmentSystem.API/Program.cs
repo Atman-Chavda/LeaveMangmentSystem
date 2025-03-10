8ffc7a5245a77684c2d916c6fcb912be133ba63f
AtmanChavda <atmanchavda@gmail.com>
2025-03-10 20:21:23 +0530

    testing

--- LeaveMangmentSystem.API/Program.cs
+++ LeaveMangmentSystem.API/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//this should work
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
